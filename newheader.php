49249a52f1cb18f9ffac38083309e6930e503378
mitchhuan <vinnintch@gmail.com>
2023-11-03 00:53:07 +0800

    Update newheader.php

--- newheader.php
+++ newheader.php
@@ -1,3 +1,18 @@
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
+
 <header class="header">
 
    <div class="flex">
